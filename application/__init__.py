746301498b6a5264c1223b2aa698f9edfedcdc19
Abel Rasheed <abelrasheed@gmail.com>
2021-01-28 14:54:00 +0530

    Initial commit, no detection

---
+++ application/__init__.py
@@ -0,0 +1,11 @@
+from flask_cors import CORS
+from flask import Flask
+from flask_restful import Api
+
+
+
+app = Flask(__name__)
+CORS(app)
+api = Api(app)
+
+from application.routes import image_return,test
